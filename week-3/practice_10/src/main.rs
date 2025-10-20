2dd1ed35202b8ed2013b4e2d80568fdd60396a98
emelie4321 <email>
2025-10-20 19:37:00 +0100

    (Week4

--- week-3/practice_10/src/main.rs
+++ week-3/practice_10/src/main.rs
@@ -2,7 +2,6 @@ fn main() {
     let sum = 5550 + 7310;
     println!("The sum of 5550 and 7310 = {}", sum);
 
-    let difference:f32 = 95.5 - 4.3;
     println!("The difference of 95.5 and 4.3 = {}", difference);
 
     let product:u32 = 4 * 30;
